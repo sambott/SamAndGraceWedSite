a2d1d4cdaad2b4601faef2d00bc9907edd1e818f
Sam Bott <sam.bott@gmail.com>
2015-02-15 16:09:20 +0000

    added route for giftlist

--- SamAndGrace/Web/App_Start/RouteConfig.cs
+++ SamAndGrace/Web/App_Start/RouteConfig.cs
@@ -14,6 +14,11 @@ namespace Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "GiftList",
+                url: "giftlist/{*anything}",
+                defaults: new { controller = "Home", action = "GiftList" }
+            );
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
