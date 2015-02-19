322bc3017dd00356a44847b79fc9a06bef3bcd55
Sam Bott <sam.bott@gmail.com>
2015-02-19 22:17:58 +0000

    Added few more skeleton pages and a day counter

--- SamAndGrace/Web.Tests/Controllers/HomeControllerTest.cs
+++ SamAndGrace/Web.Tests/Controllers/HomeControllerTest.cs
@@ -52,5 +52,43 @@ namespace Web.Tests.Controllers
             Assert.IsNotNull(result);
         }
 
+        [Test]
+        public void TaxisExists()
+        {
+            var controller = new HomeController();
+            var result = controller.Taxis() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HotelsExists()
+        {
+            var controller = new HomeController();
+            var result = controller.Hotels() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void DaysTilWeddingOnTheDayIsZero()
+        {
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDateTime), Is.EqualTo(0));
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDate), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void BeforeTheWeddingItShouldShowDaysToGo()
+        {
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDateTime.AddDays(-1)), Is.EqualTo(1));
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDate.AddMinutes(-1)), Is.EqualTo(1));
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDateTime.AddDays(-5)), Is.EqualTo(5));
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDateTime.AddDays(-80)), Is.EqualTo(80));
+        }
+
+        [Test]
+        public void AfterWeddingItKeepsAtZero()
+        {
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDate.AddDays(1)), Is.EqualTo(0));
+            Assert.That(HomeController.DaysTilWedding(HomeController.WeddingDateTime.AddDays(8)), Is.EqualTo(0));
+        }
     }
 }
--- SamAndGrace/Web/Controllers/HomeController.cs
+++ SamAndGrace/Web/Controllers/HomeController.cs
@@ -8,8 +8,19 @@ namespace Web.Controllers
 {
     public class HomeController : Controller
     {
+        public static readonly DateTime WeddingDateTime = new DateTime(2015, 05, 03, 14, 30, 00).ToUniversalTime();
+
+        public static DateTime WeddingDate
+        {
+            get
+            {
+                return WeddingDateTime.Date;
+            }
+        }
+
         public ActionResult Index()
         {
+            ViewBag.DaysToGo = DaysTilWedding(DateTime.UtcNow);
             return View();
         }
 
@@ -28,9 +39,25 @@ namespace Web.Controllers
             return View();
         }
 
+        public ActionResult Taxis()
+        {
+            return View();
+        }
+
+        public ActionResult Hotels()
+        {
+            return View();
+        }
+
         public ActionResult MusicSuggestions()
         {
             return View();
+        }
+
+        public static int DaysTilWedding(DateTime utcNow)
+        {
+            var weddingInterval = (WeddingDate - utcNow.Date).Days;
+            return weddingInterval < 0 ? 0 : weddingInterval;
         }
     }
 }
